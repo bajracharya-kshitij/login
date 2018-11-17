be482efa58476f9ce337e0d1a9dca0359429a18b
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2018-11-17 19:35:01 +0545

    Add SecUser entity and registration form

--- src/main/java/np/com/login/controller/SecUserController.java
+++ src/main/java/np/com/login/controller/SecUserController.java
@@ -1,13 +1,24 @@
 package np.com.login.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import np.com.login.model.SecUser;
 
 @Controller
 public class SecUserController {
 
-	@RequestMapping("/register")
-	public String register() {
+	@RequestMapping(value = {"/register"}, method = RequestMethod.GET)
+	public String getRegistrationPage(Model model) {
+		model.addAttribute("secUser", new SecUser());
 		return "register";
+	}
+	
+	@RequestMapping(value = {"/register"}, method = RequestMethod.POST)
+	public String register() {
+		System.out.println("Registration completed!!!");
+		return "redirect:/register";
 	}
 }
